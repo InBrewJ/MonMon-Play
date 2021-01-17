48bd92558fb40d14c42c31eb277bed5d9fc45e7d
Jason Brewer <jason@urawizard.com>
2021-01-17 18:03:55 +0000

    MWM-14 fleshing out SPOG (#2)
    * MWM-14 adds horrible unrounded MAX amount left per day/week + yearly outgoings/takehome/surplus
    
    * MWM-14 adds 2dp rounding to spog values
    
    * Adds outgoingRepository.rents() so we can calculate percentageIncomeAsRent for the SPOG
    
    * updates calculateDaysBetweenPaydays to actually return a real value each month rather than hardcoding 31

--- app/controllers/SpogController.java
+++ app/controllers/SpogController.java
@@ -8,6 +8,7 @@ import play.mvc.Result;
 import viewModels.Spog;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import static helpers.MathHelpers.round2;
@@ -32,10 +33,12 @@ public class SpogController extends Controller {
     public Result index(final Http.Request request) throws ExecutionException, InterruptedException {
         Float outgoingTotal = getTotalOutgoings(repoListToList(outgoingRepository.list()));
         Float incomingTotal = getTotalIncomings(repoListToList(incomingRepository.list()));
+        List<Outgoing> rents = repoListToList(outgoingRepository.rents());
+        Float rentCost = rents.get(0).cost;
         Float surplus = round2(incomingTotal - outgoingTotal);
         int suggestedIncomeAsSavings = 20;
         int nextPayDay = incomingRepository.getNextPayDay();
-        Spog spogVm = new Spog(surplus, nextPayDay, suggestedIncomeAsSavings, incomingTotal);
+        Spog spogVm = new Spog(surplus, nextPayDay, suggestedIncomeAsSavings, incomingTotal, outgoingTotal, rentCost);
         return ok(views.html.spog.render(spogVm, request));
     }
 }
--- app/viewModels/Spog.java
+++ app/viewModels/Spog.java
@@ -2,29 +2,52 @@ package viewModels;
 
 import java.time.LocalDate;
 
+import static helpers.MathHelpers.round2;
+
 public class Spog {
     private final Float surplus;
     private final int nextPayday;
     private final int percentageIncomeAsSavings;
+    private final Float percentageIncomeAsRent;
     private final int daysUntilNextPayday;
+    private final int daysBetweenPaydays;
     private final Float incomingTotal;
+    private final Float maxPerDay;
+    private final Float maxPerWeek;
+    private final Float yearlySurplus;
+    private final Float yearlyOutgoings;
     private final LocalDate nextPayDate;
+    private final Float yearlyTakehome;
+    private final Float rentCost;
 
-    public Spog(Float surplus, int nextPayday, int percentageIncomeAsSavings, Float incomingTotal) {
-        this.surplus = surplus;
+    public Spog(Float surplus, int nextPayday, int percentageIncomeAsSavings, Float incomingTotal, Float outgoingTotal, Float
+                rentCost) {
+        LocalDate now = LocalDate.now();
+        this.rentCost = rentCost;
+        this.surplus = round2(surplus);
         this.nextPayday = nextPayday;
-        this.daysUntilNextPayday = this.calculateDaysUntilNextPayday(nextPayday, LocalDate.now());
-        this.nextPayDate = this.calculateNextPaydayDate(nextPayday, LocalDate.now());
+        this.daysUntilNextPayday = this.calculateDaysUntilNextPayday(nextPayday, now);
+        this.nextPayDate = this.calculateNextPaydayDate(nextPayday, now);
         this.percentageIncomeAsSavings = percentageIncomeAsSavings;
         this.incomingTotal = incomingTotal;
+        this.daysBetweenPaydays = this.calculateDaysBetweenPaydays(nextPayday, now);
+        this.maxPerDay = round2(surplus / this.daysBetweenPaydays);
+        this.maxPerWeek = round2(this.maxPerDay * 7);
+        this.yearlySurplus = round2(surplus * 12);
+        this.yearlyOutgoings = round2(outgoingTotal * 12);
+        this.yearlyTakehome = round2(incomingTotal * 12);
+        this.percentageIncomeAsRent = this.calculatePercentageIncomeAsRent();
     }
 
-    public int getDaysUntilNextPayday() {
-        return daysUntilNextPayday;
+    private Float calculatePercentageIncomeAsRent() {
+        return round2((this.rentCost / this.incomingTotal) * 100);
     }
 
-    public LocalDate getNextPayDate() {
-        return nextPayDate;
+    private int calculateDaysBetweenPaydays(int nextPayday, LocalDate now) {
+        int daysUntilNextPayday = this.calculateDaysUntilNextPayday(nextPayday, now);
+        int daysSinceLastPayday = this.calculateDaysSinceLastPayday(nextPayday, now);
+        int daysBetweenPayDays = daysUntilNextPayday + daysSinceLastPayday;
+        return daysBetweenPayDays;
     }
 
     private LocalDate calculateNextPaydayDate(int nextPayday, LocalDate from) {
@@ -49,6 +72,34 @@ public class Spog {
         return count;
     }
 
+    private int calculateDaysSinceLastPayday(int lastPayday, LocalDate from) {
+        LocalDate possiblePayDate = from;
+        boolean found = false;
+        int count = 0;
+        while(!found) {
+            possiblePayDate = possiblePayDate.minusDays(1);
+            count++;
+            if (possiblePayDate.getDayOfMonth() == lastPayday) found = true;
+        }
+        return count;
+    }
+
+    public Float getPercentageIncomeAsRent() {
+        return percentageIncomeAsRent;
+    }
+
+    public int getDaysUntilNextPayday() {
+        return daysUntilNextPayday;
+    }
+
+    public LocalDate getNextPayDate() {
+        return nextPayDate;
+    }
+
+    public int getDaysBetweenPaydays() {
+        return daysBetweenPaydays;
+    }
+
     public Float getIncomingTotal() {
         return incomingTotal;
     }
@@ -65,4 +116,27 @@ public class Spog {
         return percentageIncomeAsSavings;
     }
 
+    public Float getMaxPerDay() {
+        return maxPerDay;
+    }
+
+    public Float getMaxPerWeek() {
+        return maxPerWeek;
+    }
+
+    public Float getYearlySurplus() {
+        return yearlySurplus;
+    }
+
+    public Float getYearlyOutgoings() {
+        return yearlyOutgoings;
+    }
+
+    public Float getYearlyTakehome() {
+        return yearlyTakehome;
+    }
+
+    public Float getRentCost() {
+        return rentCost;
+    }
 }
