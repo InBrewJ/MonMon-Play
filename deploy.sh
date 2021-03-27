be1654f6b7ef926f6471686d95d54dd70eaf407d
InBrewJ <jason@urawizard.com>
2021-03-27 13:05:01 +0000

    MWM-11 adds the deploy/run scripts to the circleci config - it absolutely will not work yet, need to set up SSH keys on DigitalOcean

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,5 @@
 
 ## Move the zipped folder from /target/universal/monmon-play-java--1.0-SNAPSHOT.zip
 
-scp ./target/universal/monmon-play-java--1.0-SNAPSHOT.zip circle@159.65.215.22:/home/circle/app
-
-## Then unzip
-## unzip monmon-play-java--1.0-SNAPSHOT.zip -d monmon-play
-
-
-## Then run with something like
-## WHERE THE SECRET SHOULD BE CHANGED!!!
-## monmon-play-java-/bin/monmon-play-java- -Dplay.http.secret.key="41fa^pSzvve:iunSpW5HproHJ^EF5Ml1o[1Wfbc[[gOD?jHC;[t?j9Ms0S8=ve</"
-##
-## ./monmon-play/monmon-play-java--1.0-SNAPSHOT/bin/monmon-play-java- -Dplay.http.secret.key="41fa^pSzvve:iunSpW5HproHJ^EF5Ml1o[1Wfbc[[gOD?jHC;[t?j9Ms0S8=ve</"
+du -h ./target/universal/monmon-play-java--1.0-SNAPSHOT.zip
+scp ./target/universal/monmon-play-java--1.0-SNAPSHOT.zip circle@monmon.urawizard.com:/home/circle/app
--- run_prod.sh
+++ run_prod.sh
@@ -1,4 +1,6 @@
 # Get into the /bin directory first!
 ## and unzip!
 
-./monmon-play-java- -Dplay.http.secret.key="41fa^pSzvve:iunSpW5HproHJ^EF5Ml1o[1Wfbc[[gOD?jHC;[t?j9Ms0S8=ve</"
+cd /home/circle/app
+unzip unzip monmon-play-java--1.0-SNAPSHOT.zip -d ./monmon-play
+nohup ./monmon-play/monmon-play-java--1.0-SNAPSHOT/bin/monmon-play-java- -Dplay.http.secret.key="41fa^pSzvve:iunSpW5HproHJ^EF5Ml1o[1Wfbc[[gOD?jHC;[t?j9Ms0S8=ve</" > ./monmon.out 2> ./monmon.err &
