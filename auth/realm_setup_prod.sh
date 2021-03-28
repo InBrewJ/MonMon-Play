1957497d0d3d9b104caac41f295d383361aa8b01
InBrewJ <jason@urawizard.com>
2021-03-28 21:14:49 +0100

    MWW-11 fixes findLastPaydayDate, it will no longer double count outgoings if payday = 31 and last month doesnt have 31 days. Adds a setup script to identity.urawizard.com

--- auth/realm_setup_prod.sh
+++ auth/realm_setup_prod.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # All credit to a comment from Thomas in his post:
-# https://suedbroecker.net/2020/08/04/how-to-create-a-new-realm-with-the-keycloak-rest-api/
+# httpss://suedbroecker.net/2020/08/04/how-to-create-a-new-realm-with-the-keycloak-rest-api/
 
 # Set the needed parameter
 USER=admin
-PASSWORD=admin
+PASSWORD=XXX
 GRANT_TYPE=password
 CLIENT_ID=admin-cli
-INGRESSURL="localhost:8080"
+INGRESSURL="identity.urawizard.com"
 
 echo "------------------------------------------------------------------------"
-echo "Your INGRESSURL for Keycloak: http://$INGRESSURL"
+echo "Your INGRESSURL for Keycloak: https://$INGRESSURL"
 echo "------------------------------------------------------------------------"
 echo ""
 
@@ -20,7 +20,7 @@ echo "------------------------------------------------------------------------"
 echo "Get the bearer token from Keycloak"
 echo "------------------------------------------------------------------------"
 echo ""
-access_token=$( curl -d "client_id=$CLIENT_ID" -d "username=$USER" -d "password=$PASSWORD" -d "grant_type=$GRANT_TYPE" "http://$INGRESSURL/auth/realms/master/protocol/openid-connect/token" | sed -n 's|.*"access_token":"\([^"]*\)".*|\1|p')
+access_token=$( curl -d "client_id=$CLIENT_ID" -d "username=$USER" -d "password=$PASSWORD" -d "grant_type=$GRANT_TYPE" "https://$INGRESSURL/auth/realms/master/protocol/openid-connect/token" | sed -n 's|.*"access_token":"\([^"]*\)".*|\1|p')
 
 # Create the realm in Keycloak
 echo "------------------------------------------------------------------------"
@@ -28,13 +28,13 @@ echo "Create the realm in Keycloak"
 echo "------------------------------------------------------------------------"
 echo ""
 
-result=$(curl -d @./monmon-realm.json -H "Content-Type: application/json" -H "Authorization: bearer $access_token" "http://$INGRESSURL/auth/admin/realms")
+result=$(curl -d @./monmon-realm.json -H "Content-Type: application/json" -H "Authorization: bearer $access_token" "https://$INGRESSURL/auth/admin/realms")
 
 if [ "$result" = "" ]; then
 echo "------------------------------------------------------------------------"
 echo "The realm is created."
 echo "Open following link in your browser:"
-echo "http://$INGRESSURL/auth/admin/master/console/#/realms/monmon"
+echo "https://$INGRESSURL/auth/admin/master/console/#/realms/monmon"
 echo "------------------------------------------------------------------------"
 else
 echo "------------------------------------------------------------------------"
